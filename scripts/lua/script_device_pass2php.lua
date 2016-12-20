9a2eeb3ddbf4443143bb8a850ca063c852ae8c71
Guy Verschuere <guy@egregius.be>
2016-12-20 05:00:14 +0100

    Update script_device_pass2php.lua

--- scripts/lua/script_device_pass2php.lua
+++ scripts/lua/script_device_pass2php.lua
@@ -1,9 +1,11 @@
-JSON=loadfile('/volume1/@appstore/domoticz/var/scripts/JSON.lua')()
-base64=loadfile('/volume1/@appstore/domoticz/var/scripts/base64.lua')()
-c=base64.encode(JSON:encode(devicechanged))
-s=base64.encode(JSON:encode(otherdevices))
-i=base64.encode(JSON:encode(otherdevices_idx))
-t=base64.encode(JSON:encode(otherdevices_lastupdate))
+c = ''
+s = ''
+i = ''
+t = ''
+for k,v in pairs(devicechanged) do c = c..k.."|"..v.."#" end
+for k,v in pairs(otherdevices) do s = s..k.."|"..v.."#" end
+for k,v in pairs(otherdevices_idx) do i = i..k.."|"..v.."#" end
+for k,v in pairs(otherdevices_lastupdate) do t = t..k.."|"..v.."#" end
 --os.execute('/volume1/web/secure/pass2php.php "'..c..'" "'..s..'" "'..i..'" "'..t..'" &')
 os.execute('curl -s --data "c='..c..'&s='..s..'&i='..i..'&t='..t..'" http://127.0.0.1/secure/pass2php.php &')
 commandArray={}
