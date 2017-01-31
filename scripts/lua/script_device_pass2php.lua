8cb817cca3f835974c8be5969e70ec346de97728
Guy Verschuere <guy@egregius.be>
2017-01-31 07:18:14 +0100

    Update script_device_pass2php.lua

--- scripts/lua/script_device_pass2php.lua
+++ scripts/lua/script_device_pass2php.lua
@@ -1,12 +1,5 @@
 c = ''
-s = ''
-i = ''
-t = ''
 for k,v in pairs(devicechanged) do c = c..k.."|"..v.."#" end
-for k,v in pairs(otherdevices) do s = s..k.."|"..v.."#" end
-for k,v in pairs(otherdevices_idx) do i = i..k.."|"..v.."#" end
-for k,v in pairs(otherdevices_lastupdate) do t = t..k.."|"..v.."#" end
---os.execute('/volume1/web/secure/pass2php.php "'..c..'" "'..s..'" "'..i..'" "'..t..'" &')
-os.execute('curl -s --data "c='..c..'&s='..s..'&i='..i..'&t='..t..'" http://127.0.0.1/secure/pass2php.php &')
+os.execute('curl -s -d "c='..c..'" http://127.0.0.1/secure/pass2php.php &')
 commandArray={}
 return commandArray
