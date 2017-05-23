3eabf76e48ca1b7ee05fa3fa8765e72e7ac3a6b9
Guy Verschuere <guy@egregius.be>
2017-05-23 13:44:17 +0200

    Update script_device_pass2php.lua

--- scripts/lua/script_device_pass2php.lua
+++ scripts/lua/script_device_pass2php.lua
@@ -1,6 +1,6 @@
-c = ''
-for k,v in pairs(devicechanged) do c = c..k.."|"..v.."#" end
---os.execute('/volume1/web/secure/pass2php.php "'..c..'" &')
-os.execute('curl -s -d "c='..c..'" http://127.0.0.1/secure/pass2php.php &')
+for d,s in pairs(devicechanged)
+do
+os.execute('curl -X POST -d "d='..d.."&s="..s..'" http://127.0.0.1/secure/pass2php.php &')
+end
 commandArray={}
 return commandArray
