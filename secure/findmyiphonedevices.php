2e77baedf391dcbe4c89163d9e4b3326659a9f8b
Guy Verschuere <guy@egregius.be>
2017-05-23 13:58:07 +0200

    Update findmyiphonedevices.php

--- secure/findmyiphonedevices.php
+++ secure/findmyiphonedevices.php
@@ -1,13 +1,16 @@
 <?PHP
-	error_reporting(0);
-	header("Content-type: text/html; charset=utf-8");
-	require_once "settings.php";
-	require_once "functions.php";
-	require_once "findmyiphone.php";
-	try {
-		$fmi = new FindMyiPhone($appleid, $applepass);
-	} catch (Exception $e) {
-		print "Error: ".$e->getMessage();
-		exit;
-	}
-	$fmi->printDevices();
+error_reporting(0);
+header("Content-type: text/html; charset=utf-8");
+require_once "settings.php";
+require_once "functions.php";
+require_once "findmyiphone.php";
+$appledevice='AcxBCrcbnf39ozym/jmrnD3Et3FtbG8mH5iaqI+2fWLrppKjayZWsOHYVNSUzmWV';
+$appleid='you@me.com';
+$applepass='yourpassword';
+try {
+	$fmi = new FindMyiPhone($appleid, $applepass);
+} catch (Exception $e) {
+	print "Error: ".$e->getMessage();
+	exit;
+}
+$fmi->printDevices();
