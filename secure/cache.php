8f5baa7e46778810e332255a90350a3ab0437d1e
Guy Verschuere <guy@egregius.be>
2017-09-08 08:44:59 +0200

    Update cache.php

--- secure/cache.php
+++ secure/cache.php
@@ -1,3 +1,6 @@
 <?php
-if(isset($_REQUEST['fetch']))echo apcu_fetch($_REQUEST['fetch']);
-elseif(isset($_REQUEST['store'])&&isset($_REQUEST['value']))apcu_store($_REQUEST['store'],$_REQUEST['value']);
+if(isset($_REQUEST['fetch']))echo status($_REQUEST['fetch']);
+elseif(isset($_REQUEST['store'])&&isset($_REQUEST['value']))setstatus($_REQUEST['store'],$_REQUEST['value']);
+function setstatus($name,$value){file_put_contents('/var/log/cache/s'.$name.'.cache',$value);}
+function status($name){if(file_exists('/var/log/cache/s'.$name.'.cache'))return file_get_contents('/var/log/cache/s'.$name.'.cache');else return 0;}
+?>
