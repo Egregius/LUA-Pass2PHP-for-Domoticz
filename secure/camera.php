767beca62c672398c5d7d04608a0c4ea228d9280
Guy Verschuere <guy@egregius.be>
2017-09-08 08:46:11 +0200

    Update camera.php

--- secure/camera.php
+++ secure/camera.php
@@ -1,7 +1,11 @@
 <?php
 $d=array();
-$d['Weg']=apcu_fetch('sWeg');
-$d['meldingen']=apcu_fetch('smeldingen');
-$d['poort']=apcu_fetch('spoort');
-$d['tpoort']=apcu_fetch('tpoort');
+$d['Weg']=status('Weg');
+$d['meldingen']=status('meldingen');
+$d['poort']=status('poort');
+$d['tpoort']=timestamp('poort');
 echo serialize($d);
+
+function status($name){if(file_exists('/var/log/cache/s'.$name.'.cache'))return file_get_contents('/var/log/cache/s'.$name.'.cache');else return 0;}
+function timestamp($name){if(file_exists('/var/log/cache/s'.$name.'.cache'))return filemtime('/var/log/cache/s'.$name.'.cache');else return 0;}
+?>
