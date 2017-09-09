e3cfa221ef72a820f83edb11ef8fd5019f3c651e
Guy Verschuere <guy@egregius.be>
2017-09-09 16:20:29 +0200

    Update _cron604800.php

--- secure/_cron604800.php
+++ secure/_cron604800.php
@@ -4,25 +4,32 @@ if($domoticz){
 	foreach($domoticz['result'] as $dom){
 		$name=$dom['Name'];
 		if(isset($dom['SwitchType']))$switchtype=$dom['SwitchType'];else $switchtype='none';
-		apcu_store('t'.$name,strtotime($dom['LastUpdate']));
-		apcu_store('i'.$name,$dom['idx']);
-		if($dom['Type']=='Temp')apcu_store('s'.$name,str_replace(' C','',$dom['Data']));
-		elseif($dom['TypeImg']=='current')apcu_store('s'.$name,str_replace(' Watt','',$dom['Usage']));
+		settimestamp($name,strtotime($dom['LastUpdate']));
+		setidx($name,$dom['idx']);
+		if($dom['Type']=='Temp')setstatus($name,str_replace(' C','',$dom['Data']));
+		elseif($dom['TypeImg']=='current')setstatus($name,str_replace(' Watt','',$dom['Data']));
+		elseif($name=='luifel')setstatus($name,str_replace('%','',$dom['Level']));
 		elseif($switchtype=='Dimmer'){
-			if($dom['Data']=='Off')apcu_store('s'.$name,'Off');
-			else apcu_store('s'.$name,filter_var($dom['Data'],FILTER_SANITIZE_NUMBER_INT));
+			if($dom['Data']=='Off')setstatus($name,'Off');
+			else setstatus($name,filter_var($dom['Data'],FILTER_SANITIZE_NUMBER_INT));
 		}
-		else apcu_store('s'.$name,$dom['Data']);
+		else setstatus($name,$dom['Data']);
 	}
 }
-if(!apcu_exists('Weg')){apcu_store('Weg',0);$Weg=0;}
-$item='meldingen';if(!apcu_exists('s'.$item)){apcu_store('s'.$item,'On');apcu_store('t'.$item,1);}
-$item='lichten_auto';if(!apcu_exists('s'.$item)){apcu_store('s'.$item,'On');apcu_store('t'.$item,1);}
-$item='heating';if(!apcu_exists('s'.$item)){apcu_store('s'.$item,'On');apcu_store('t'.$item,1);}
-$item='heatingmanual';if(!apcu_exists('s'.$item)){apcu_store('s'.$item,'On');apcu_store('t'.$item,1);}
-$item='living';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',17);apcu_store('t'.$item.'_set',1);}
-$item='badkamer';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',12);apcu_store('t'.$item.'_set',1);}
-$item='kamer';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',12);apcu_store('t'.$item.'_set',1);}
-$item='tobi';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',12);apcu_store('t'.$item.'_set',1);}
-$item='alex';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',12);apcu_store('t'.$item.'_set',1);}
-$item='diepvries';if(!apcu_exists('s'.$item.'_set')){apcu_store('s'.$item.'_set',-18);}
+if(!file_exists('/var/log/cache-Weg')){setstatus('Weg',0);$Weg=0;}
+$item='meldingen';if(status($item)==0)setstatus($item,'On');
+$item='lichten_auto';if(status($item)==0)setstatus($item,'On');
+$item='living';if(status($item)==0)setstatus($item.'_set',17);
+$item='badkamer';if(status($item)==0)setstatus($item.'_set',12);
+$item='kamer';if(status($item)==0)setstatus($item.'_set',12);
+$item='tobi';if(status($item)==0)setstatus($item.'_set',12);
+$item='alex';if(status($item)==0)setstatus($item.'_set',12);
+$item='diepvries';if(status($item)==0)setstatus($item.'_set',-18);
+if(in_array(date('n'),array(5,6,7,8,9))){
+	$item='heating';if(status($item)==0)setstatus('s'.$item,'Off');
+	$item='heatingmanual';if(status($item)==0)setstatus('s'.$item,'Off');
+}else{
+	$item='heating';if(status($item)==0)setstatus('s'.$item,'On');
+	$item='heatingmanual';if(status($item)==0)setstatus('s'.$item,'On');
+}
+?>
