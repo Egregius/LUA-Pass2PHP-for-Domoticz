80b6a2f058d21cb0127925557180ee62ce5521f1
Guy Verschuere <guy@egregius.be>
2017-11-21 14:17:27 +0100

    Update pass2php.php

--- secure/pass2php.php
+++ secure/pass2php.php
@@ -2,31 +2,37 @@
 require('/var/www/html/secure/settings.php');
 error_reporting(E_ALL);ini_set("display_errors","on");
 define('time',$_SERVER['REQUEST_TIME']);
-if(status('lichten_auto')=='On')$auto=true;else $auto=false;
-$Weg=status('Weg');
-if(status('meldingen')=='On')$meldingen=true;else $meldingen=false;
-$zon=status('zon');
-$zongarage=700;
-$zonkeuken=60;
-$zoninkom=15;
-$zonmedia=20;
-$smappeeip='192.168.2.177';
+if(apcu_fetch('lichten_auto')=='On')$auto=true;else $auto=false;
+$Weg=apcu_fetch('Weg');
+if(apcu_fetch('meldingen')=='On')$meldingen=true;else $meldingen=false;
+$zon=apcu_fetch('zon');
 if($_SERVER['REQUEST_METHOD']=='POST'){
 		$device=$_POST['d'];
 		$status=$_POST['s'];
 		if(@include '/var/www/html/secure/pass2php/'.$device.'.php'){
 			lg($device.' = '.$status);
 			if(in_array($device,array('brander','badkamervuur'))){
-				$prev=status($device);
+				$prev=apcu_fetch($device);
 				if($status!=$prev)setstatus('tt'.$device,time);
 			}
 			if(in_array($device,array('eettafel','zithoek','kamer','tobi','alex'))){
 				if($status=='Off'){
-					setstatus($device,'Off');
+					setstatus($device,0);
+				}elseif($status=='On'){
+					setstatus($device,100);
+				}else{
+					$status=filter_var($status,FILTER_SANITIZE_NUMBER_INT);
+					setstatus($device,$status);
 				}
-				else{lg('else'.$status); setstatus($device,filter_var($status,FILTER_SANITIZE_NUMBER_INT));}
-			}
-			else setstatus($device,$status);
+			}elseif($device=='luifel'){
+				if($status=='Closed'){
+					setstatus($device,100);
+				}elseif($status=='Open'){
+					setstatus($device,0);
+				}else{
+					$status=filter_var($status,FILTER_SANITIZE_NUMBER_INT);
+					setstatus($device,$status);				}
+			}else setstatus($device,$status);
 		}
 }
 if(timestamp('cron5')<time-4){
@@ -35,13 +41,13 @@ if(timestamp('cron5')<time-4){
 		settimestamp('cron604800');
 		include('/var/www/html/secure/_cron604800.php');
 	}
-	if(timestamp('cron28800')<time-27790){
-		settimestamp('cron28800');
-		include('/var/www/html/secure/_cron28800.php');
-	}
-	if(timestamp('cron180')<time-130){
-		settimestamp('cron180');
-		include('/var/www/html/secure/_cron180.php');
+	if(timestamp('cron120')<time-118){
+		settimestamp('cron120');
+		include('/var/www/html/secure/_cron120.php');
+		if(timestamp('cron28800')<time-27790){
+			settimestamp('cron28800');
+			include('/var/www/html/secure/_cron28800.php');
+		}
 	}
 	if(timestamp('cron60')<time-58){
 		settimestamp('cron60');
