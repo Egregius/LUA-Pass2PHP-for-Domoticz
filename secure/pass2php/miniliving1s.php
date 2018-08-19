8d0a4530809d525742af3c5de2f3ed210df22f6d
Guy Verschuere <guy@egregius.be>
2018-08-19 06:49:38 +0200

    Update miniliving1s.php

--- secure/pass2php/miniliving1s.php
+++ secure/pass2php/miniliving1s.php
@@ -1,36 +1,38 @@
 <?php
+/*apcu_store('denoninput','TUNER');
 $ctx=stream_context_create(array('http'=>array('timeout'=>2)));
-for($k=1;$k<=200;$k++){
-	if(status('denon')!='On'){
+for($k=1;$k<=100;$k++){
+	if(apcu_fetch('denon')!='On'){
 		sw('denon','On');
 		usleep(500000);
 	}else break;
 }
-for($k=1;$k<=200;$k++){
+for($k=1;$k<=100;$k++){
 	$denon="";
-	$denon=json_decode(json_encode(simplexml_load_string(file_get_contents('http://192.168.2.4/goform/formMainZone_MainZoneXml.xml?_='.time(),false,$ctx))),true);
+	$denon=@json_decode(@json_encode(@simplexml_load_string(@file_get_contents('http://192.168.2.204/goform/formMainZone_MainZoneXml.xml?_='.time(),false,$ctx))),true);
 	if($denon['ZonePower']['value']!='ON'){
-		file_get_contents('http://192.168.2.4/MainZone/index.put.asp?cmd0=PutZone_OnOff%2FON&cmd1=aspMainZone_WebUpdateStatus%2F',false,$ctx);
+		file_get_contents('http://192.168.2.204/MainZone/index.put.asp?cmd0=PutZone_OnOff%2FON&cmd1=aspMainZone_WebUpdateStatus%2F',false,$ctx);
 		usleep(500000);
 	}else break;
 }
-for($k=1;$k<=200;$k++){
+for($k=1;$k<=100;$k++){
 	$denon="";
-	$denon=json_decode(json_encode(simplexml_load_string(file_get_contents('http://192.168.2.4/goform/formMainZone_MainZoneXml.xml?_='.time().'&ZoneName=ZONE2',false,$ctx))),true);
+	$denon=@json_decode(@json_encode(@simplexml_load_string(@file_get_contents('http://192.168.2.204/goform/formMainZone_MainZoneXml.xml?_='.time().'&ZoneName=ZONE2',false,$ctx))),true);
 	if($denon['ZonePower']['value']!='ON'){
-		file_get_contents('http://192.168.2.4/MainZone/index.put.asp?cmd0=PutZone_OnOff%2FON&cmd1=aspMainZone_WebUpdateStatus%2F&ZoneName=ZONE2',false,$ctx);
+		file_get_contents('http://192.168.2.204/MainZone/index.put.asp?cmd0=PutZone_OnOff%2FON&cmd1=aspMainZone_WebUpdateStatus%2F&ZoneName=ZONE2',false,$ctx);
 		usleep(500000);
 	}else break;
 }
-for($k=1;$k<=200;$k++){
+for($k=1;$k<=100;$k++){
 	$denon="";
-	$denon=json_decode(json_encode(simplexml_load_string(file_get_contents('http://192.168.2.4/goform/formMainZone_MainZoneXml.xml?_='.time(),false,$ctx))),TRUE);
+	$denon=@json_decode(@json_encode(@simplexml_load_string(@file_get_contents('http://192.168.2.204/goform/formMainZone_MainZoneXml.xml?_='.time(),false,$ctx))),TRUE);
 	if($denon['InputFuncSelect']['value']!='TUNER'){
-		file_get_contents('http://192.168.2.4/MainZone/index.put.asp?cmd0=PutZone_InputFunction/TUNER&cmd1=aspMainZone_WebUpdateStatus%2F');
+		file_get_contents('http://192.168.2.204/MainZone/index.put.asp?cmd0=PutZone_InputFunction/TUNER&cmd1=aspMainZone_WebUpdateStatus%2F');
 		usleep(500000);
 	}else break;
 }
-if(status('tv')!='Off')sw('tv','Off');
-if(status('tvled')!='Off')sw('tvled','Off');
-if($Weg!=0){setstatus('Weg',0);;}
+if(apcu_fetch('tv')!='Off')sw('tv','Off');
+if(apcu_fetch('tvled')!='Off')sw('tvled','Off');*/
+bosekey("POWER");
+if($Weg!=0){apcu_store('Weg',0);apcu_store('TWeg',time());}
 ?>
