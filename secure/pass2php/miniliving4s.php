4cdb9f58754018150ecc373e61993760f70b5bbb
Guy Verschuere <guy@egregius.be>
2018-08-19 06:51:28 +0200

    Update miniliving4s.php

--- secure/pass2php/miniliving4s.php
+++ secure/pass2php/miniliving4s.php
@@ -1,28 +1,34 @@
 <?php
-/*$ctx=stream_context_create(array('http'=>array('timeout'=>3)));
-$denon=json_decode(json_encode(simplexml_load_string(file_get_contents('http://192.168.2.4/goform/formMainZone_MainZoneXml.xml?_='.time,false,$ctx))),TRUE);
-if($denon){
-	$denon['MasterVolume']['value']=='--'
-		?$setvalue=-55
-		:$setvalue=$denon['MasterVolume']['value'];
-	$setvalue=$setvalue+3;if($setvalue>-10)$setvalue=-10;if($setvalue<-80)$setvalue=-80;$volume=80+$setvalue;
-	usleep(100000);
-	file_get_contents('http://192.168.2.4/MainZone/index.put.asp?cmd0=PutMasterVolumeSet/'.$setvalue.'.0',false,$ctx);
+if(apcu_fetch('denon')=='On'){
+	denon('MVUP');
+	denon('MVUP');
+	denon('MVUP');
+	denon('MVUP');
+	denon('MVUP');
+	denon('MVUP');
+}else{
+	$nowplaying=@json_decode(@json_encode(@simplexml_load_string(@file_get_contents('http://192.168.2.194:8090/now_playing'))),true);
+	if(!empty($nowplaying)){
+		if(isset($nowplaying['@attributes']['source'])){
+			if($nowplaying['@attributes']['source']!='STANDBY'){
+				$volume=@json_decode(@json_encode(@simplexml_load_string(@file_get_contents('http://192.168.2.194:8090/volume'))),true);
+				$cv=$volume['actualvolume'];
+				if($cv>80)exit;
+				if($cv>50)bosevolume($cv+5);
+				elseif($cv>30)bosevolume($cv+4);
+				elseif($cv>20)bosevolume($cv+3);
+				elseif($cv>10)bosevolume($cv+2);
+				else bosevolume($cv+1);
+			}
+		}
+	}
 }
-*/
-denon('MVUP');
-denon('MVUP');
-denon('MVUP');
-denon('MVUP');
-denon('MVUP');
-denon('MVUP');
-
 function denon($cmd){for($x=1;$x<=10;$x++)if(denontcp($cmd,$x))break;}
 function denontcp($cmd,$x){
 	$sleep=102000*$x;
-	$socket=fsockopen("192.168.2.4","23",$errno,$errstr,2);
+	$socket=@fsockopen("192.168.2.204","23",$errno,$errstr,2);
 	if($socket){fputs($socket, "$cmd\r\n");fclose($socket);usleep($sleep);return true;}
-	else{usleep($sleep);echo 'sleeping '.$sleep.'<br>';return false;}
+	else{usleep($sleep);return false;}
 }
-if($Weg!=0)setstatus('Weg',0);
+if($Weg!=0){apcu_store('Weg',0);apcu_store('TWeg',time());}
 ?>
